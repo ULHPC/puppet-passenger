db3d7104cfecbf9d154e6f89b6694d00cb5fa8aa
Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>
2016-07-13 18:55:37 +0200

    update content of 'params.pp'
    Signed-off-by: Hyacinthe Cartiaux <Hyacinthe.Cartiaux@uni.lu>

--- tests/params.pp
+++ tests/params.pp
@@ -15,12 +15,21 @@
 
 include 'passenger::params'
 
-$names = ['ensure', 'protocol', 'port', 'packagename']
+$names = ["ensure", "packagename", "version", "packagename_rake", "version_rake", "packagename_rack", "version_rack", "extra_packages", "configfile_mode", "configfile_owner", "configfile_group", "passenger_ruby", "default_user"]
 
 notice("passenger::params::ensure = ${passenger::params::ensure}")
-notice("passenger::params::protocol = ${passenger::params::protocol}")
-notice("passenger::params::port = ${passenger::params::port}")
 notice("passenger::params::packagename = ${passenger::params::packagename}")
+notice("passenger::params::version = ${passenger::params::version}")
+notice("passenger::params::packagename_rake = ${passenger::params::packagename_rake}")
+notice("passenger::params::version_rake = ${passenger::params::version_rake}")
+notice("passenger::params::packagename_rack = ${passenger::params::packagename_rack}")
+notice("passenger::params::version_rack = ${passenger::params::version_rack}")
+notice("passenger::params::extra_packages = ${passenger::params::extra_packages}")
+notice("passenger::params::configfile_mode = ${passenger::params::configfile_mode}")
+notice("passenger::params::configfile_owner = ${passenger::params::configfile_owner}")
+notice("passenger::params::configfile_group = ${passenger::params::configfile_group}")
+notice("passenger::params::passenger_ruby = ${passenger::params::passenger_ruby}")
+notice("passenger::params::default_user = ${passenger::params::default_user}")
 
 #each($names) |$v| {
 #    $var = "passenger::params::${v}"
